ec5726b52cff97110213d9ed374045656420304f
DollarAkshay <akshay95aradhya@gmail.com>
2017-11-27 01:12:00 +0530

    Making it run on any GPU

--- pokeGAN.py
+++ pokeGAN.py
@@ -16,7 +16,6 @@ slim = tf.contrib.slim
 HEIGHT, WIDTH, CHANNEL = 128, 128, 3
 BATCH_SIZE = 64
 EPOCH = 5000
-os.environ['CUDA_VISIBLE_DEVICES'] = '15'
 version = 'newPokemon'
 newPoke_path = './' + version
 
@@ -164,7 +163,6 @@ def discriminator(input, is_train, reuse=False):
 
 def train():
     random_dim = 100
-    print(os.environ['CUDA_VISIBLE_DEVICES'])
     
     with tf.variable_scope('input'):
         #real and fake image placholders
